f892dafd45067e792b8a218dfeb10eb1bd589c64
Dharmik <dharmikp.mobio@gmail.com>
2022-05-05 12:35:17 +0530

    pathsnprops

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const  home = () => {
+  return (
+   <>
+     
+   </>
+  )
+}
+
+export default home;
